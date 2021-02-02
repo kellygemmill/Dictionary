22f4d7ba592fefae01beeb93db75b1e58b67f155
Kelly Gemmill <kgemmill@ucla.edu>
2021-02-01 23:48:41 -0800

    Display base form of word

--- src/components/SingleResult.js
+++ src/components/SingleResult.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const SingleResult = ({value}) => {
+    
     return (
         <div>
             <ul>
                 <li>Surface Form: {value.word}</li>
                 <li>Reading: {value.reading}</li>
                 <li>Part of Speech: {value.partOfSpeech}</li>
-                {/* <li>Base Form: {value.baseForm}</li> */}
+                <li>Base Form: {value.baseForm !== null ? value.baseForm.word : value.word}</li>
             </ul>
         </div>
     )
