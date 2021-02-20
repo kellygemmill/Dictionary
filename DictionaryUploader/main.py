9c49edeaa202a4dbde877adc0e1da8026f25ebb2
Kelly Gemmill <54822281+kellygemmill@users.noreply.github.com>
2021-02-20 11:35:41 -0800

    Created python script to add dictionaries to database

---
+++ DictionaryUploader/main.py
@@ -0,0 +1,53 @@
+import json
+import os
+import requests
+
+
+def load_dictionary():
+    dictionary_folder = r'Dictionaries/jmdict_english'
+    info_file_name = 'index.json'
+    with open(f'{dictionary_folder}/{info_file_name}', encoding='utf-8') as file:
+        info = json.load(file)
+        dictionary_data = {
+            "name": info["title"],
+            "type": info["type"]
+        }
+
+    post_dictionary_data(dictionary_folder, dictionary_data)
+
+
+def post_dictionary_data(folder, dictionary_data):
+    files = os.listdir(folder)
+    files_to_load = [file for file in files if file.__contains__('term_bank')]
+
+    url = 'http://localhost:8080/api'
+
+    dictionary_response = requests.post(f'{url}/dictionary', json=dictionary_data)
+    dictionary = eval(dictionary_response.content.decode('utf-8'))
+
+    for file_to_load in files_to_load:
+        file_name = folder + f'/{file_to_load}'
+
+        with open(file_name, encoding='utf-8') as file:
+            data = json.load(file)
+            words_to_add = create_words(data, dictionary)
+            x = requests.post(f'{url}/entry', json=words_to_add)
+
+
+def create_words(data, dictionary):
+    data_out = []
+    definition_sep = '; '
+    for word in data:
+        word_object = {
+            "word": word[0],
+            "reading": word[1],
+            "definition": definition_sep.join(word[5]),
+            "dictionary": dictionary
+        }
+        data_out.append(word_object)
+
+    return data_out
+
+
+if __name__ == '__main__':
+    load_dictionary()
